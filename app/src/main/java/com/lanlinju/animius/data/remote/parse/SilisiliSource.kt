842c2d3ab660239d567230fa1d221c93a2ba2333
Lanlinju <lanlinju@outlook.com>
2026-05-28 17:09:40 +0800

    fix: 修复silisi主页数据获取失败的问题

--- app/src/main/java/com/lanlinju/animius/data/remote/parse/SilisiliSource.kt
+++ app/src/main/java/com/lanlinju/animius/data/remote/parse/SilisiliSource.kt
@@ -30,8 +30,13 @@ object SilisiliSource : AnimeSource {
 
     override suspend fun getHomeData(): List<HomeBean> {
         val headers = mapOf(
-            Pair("Cookie", "silisili=on;path=/;max-age=86400;appdw=on"),
-            Pair("User-Agent", "Mozilla/5.0 (Linux; Android 6.0) Mobile"),
+            Pair("Sec-Ch-Ua-Mobile", "?1"),
+            Pair("Sec-Ch-Ua-Platform", "\"Android\""),
+            Pair("Cookie", "silisili=on;path=/;max-age=86400;appdw=on;k=260528"),
+            Pair(
+                "User-Agent",
+                "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Mobile Safari/537.36"
+            ),
         )
         val source = DownloadManager.getHtml(baseUrl, headers)
         val document = Jsoup.parse(source)
